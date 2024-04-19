49a72a4722c79690e4fe23e463b3878438bf84c5
Urryy <yura.yutsko@mail.ru>
2024-04-19 21:25:07 +0300

    Конфигурация GenericCacheRepository

--- api/DataAccess/Managers/CacheManager.cs
+++ api/DataAccess/Managers/CacheManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Caching;
+﻿using Common.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Runtime.Caching;
 
 namespace DataAccess.Managers;
 
@@ -9,6 +11,34 @@ public static class CacheManager
 
     public static string KEY_ALL_ENTITIES = "ALLENTITY";
     public static string KEY_ENTITY = "ENTITY{0}";
+
+    public const int EXPIRES_CACHE = 10;
+
+    private static async Task UpdateAllEntities<T>(T? previousEntity, T? currentEntity)
+    {
+        if (_cache.Contains(KEY_ALL_ENTITIES))
+        {
+            lock (_lockObject)
+            {
+                if (_cache.Contains(KEY_ALL_ENTITIES))
+                {
+                    List<T> items = (List<T>)_cache.Get(KEY_ALL_ENTITIES);
+                    
+                    if(previousEntity != null)
+                    {
+                        items.Remove(previousEntity);
+                    }
+                    
+                    if(currentEntity != null)
+                    {
+                        items.Add(currentEntity);
+                    }
+
+                    _cache.Set(KEY_ALL_ENTITIES, items, DateTimeOffset.UtcNow.AddMinutes(EXPIRES_CACHE));
+                }
+            }
+        }
+    }
 
     public static async Task<T> GetOrSet<T>(string key, Func<Task<T>> getItemCallback, DateTimeOffset absoluteExpiration)
     {
@@ -26,29 +56,37 @@ public static class CacheManager
         return (T)_cache.Get(key);
     }
 
-    public static async Task Remove(string key, Func<Task> removeItemCallback)
+    public static async Task Remove<T>(string key, Func<Task> removeItemCallback)
     {
         await removeItemCallback();
         if (_cache.Contains(key))
         {
+            await UpdateAllEntities(_cache.Get(key), default(T));
             _cache.Remove(key);
-        }
+        }  
     }
 
     public static async Task Update<T>(string key, Func<Task> updateItemCallback, T entity, DateTimeOffset absoluteExpiration)
     {
+        T previousEntity = default;
+
         await updateItemCallback();
+
         if (_cache.Contains(key))
         {
+            previousEntity = (T?)_cache.Get(key);
             _cache.Remove(key);
         }
 
         _cache.Set(key, entity, absoluteExpiration);
+
+        await UpdateAllEntities<T>(previousEntity, entity);
     }
 
     public static async Task Create<T>(string key, Func<Task> updateItemCallback, T entity, DateTimeOffset absoluteExpiration)
     {
         await updateItemCallback();
         _cache.Set(key, entity, absoluteExpiration);
+        await UpdateAllEntities<T>(default(T), entity);
     }
 }
--- api/DataAccess/Repository/Implemintations/GenericCacheRepository.cs
+++ api/DataAccess/Repository/Implemintations/GenericCacheRepository.cs
@@ -8,7 +8,6 @@ namespace DataAccess.Repository.Implemintations;
 
 public class GenericCacheRepository<T> : GenericRepository<T>, ICacheRepository<T> where T : Entity<T>
 {
-    public const int EXPIRES_CACHE = 10;
 
     public GenericCacheRepository(DatabaseContext context) : base(context)
     {
@@ -17,29 +16,29 @@ public class GenericCacheRepository<T> : GenericRepository<T>, ICacheRepository<
     public async Task<List<T>> GetAllFromCacheAsync(IFetch<T> fetch = null, bool isTracked = true)
     {
         return await CacheManager.GetOrSet(CacheManager.KEY_ALL_ENTITIES,
-            () => GetAllAsync(fetch, isTracked), DateTimeOffset.UtcNow.AddMinutes(EXPIRES_CACHE));
+            () => GetAllAsync(fetch, isTracked), DateTimeOffset.UtcNow.AddMinutes(CacheManager.EXPIRES_CACHE));
     }
 
     public async Task<T?> GetByIdFromCacheAsync(Guid id, IFetch<T> fetch = null)
     {
         return await CacheManager.GetOrSet(string.Format(CacheManager.KEY_ENTITY, id),
-            () => GetByIdAsync(id, fetch), DateTimeOffset.UtcNow.AddMinutes(EXPIRES_CACHE));
+            () => GetByIdAsync(id, fetch), DateTimeOffset.UtcNow.AddMinutes(CacheManager.EXPIRES_CACHE));
     }
 
     public async Task DeleteEntityAsync(Guid id)
     {
-        await CacheManager.Remove(string.Format(CacheManager.KEY_ENTITY, id), () => DeleteAsync(id));
+        await CacheManager.Remove<T>(string.Format(CacheManager.KEY_ENTITY, id), () => DeleteAsync(id));
     }
 
     public async Task UpdateEntityAsync(T entity)
     {
         await CacheManager.Update(string.Format(CacheManager.KEY_ENTITY, entity.Id),
-            () => UpdateAsync(entity), entity, DateTimeOffset.UtcNow.AddMinutes(EXPIRES_CACHE));
+            () => UpdateAsync(entity), entity, DateTimeOffset.UtcNow.AddMinutes(CacheManager.EXPIRES_CACHE));
     }
 
     public async Task CreateEntityAsync(T entity)
     {
         await CacheManager.Create(string.Format(CacheManager.KEY_ENTITY, entity.Id),
-            () => AddAsync(entity), entity, DateTimeOffset.UtcNow.AddMinutes(EXPIRES_CACHE));
+            () => AddAsync(entity), entity, DateTimeOffset.UtcNow.AddMinutes(CacheManager.EXPIRES_CACHE));
     }
 }
