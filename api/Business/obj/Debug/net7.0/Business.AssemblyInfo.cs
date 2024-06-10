f77f31f38dd4de24d5c613be1b16b6b4927f8e23
Яцко Юрий Владимирович <yatsko_yu@st.by>
2024-06-10 10:08:26 +0300

    d

--- api/Business/obj/Debug/net7.0/Business.AssemblyInfo.cs
+++ api/Business/obj/Debug/net7.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61680f4935e3ef856434532db1e986aaa9f6a5d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8099bd447759cafb0aa3f11a75ee9c768ec3eea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- api/CrossCutting.IoC/obj/Release/net7.0/CrossCutting.IoC.AssemblyInfo.cs
+++ api/CrossCutting.IoC/obj/Release/net7.0/CrossCutting.IoC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrossCutting.IoC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61680f4935e3ef856434532db1e986aaa9f6a5d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8099bd447759cafb0aa3f11a75ee9c768ec3eea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrossCutting.IoC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrossCutting.IoC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
